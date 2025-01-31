8b31c00e7f679ddd6dc8b88f2e8ca65f20ce773d
MohamedHassan84 <mo.hassan.844@gmail.com>
2025-01-31 16:19:09 +0200

    correct installation of awscli

--- terraform/main.tf
+++ terraform/main.tf
@@ -242,6 +242,7 @@ resource "aws_db_instance" "main" {
   engine_version       = "8.0" #change as needed
   instance_class       = "db.t3.micro"
   db_name              = var.db_name
+  identifier           = var.db_name
   username             = var.db_username
   password             = var.db_password
   db_subnet_group_name = aws_db_subnet_group.main.name
